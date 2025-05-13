81ca163776b334bcaad14abf13ad88b4b2c6ad98
choichangyeon <changyon99@naver.com>
2025-05-13 20:38:07 +0900

    refactor:#1 상수화 적용

--- src/features/read/api/server-services.ts
+++ src/features/read/api/server-services.ts
@@ -1,9 +1,10 @@
 "use server";
 
+import { API_URL } from "@/constants/api";
 import { Todo } from "@/types/todo";
 
 export const getTodos = async (): Promise<Todo[]> => {
-  const res = await fetch("http://localhost:3000/todos");
+  const res = await fetch(API_URL);
   const data: Todo[] = await res.json();
 
   return data;
