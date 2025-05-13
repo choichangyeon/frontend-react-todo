3dd09c0788f339cafd2e9563ad195d173326b1b3
choichangyeon <changyon99@naver.com>
2025-05-14 03:31:47 +0900

    feat:#1 api route handler path 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -9,6 +9,7 @@ export const API_METHOD = {
 
 export const API_ROUTE_HANDLER_PATH = {
   READ: "/api/read",
+  CREATE: "/api/create",
   DELETE: (id: string) => `/api/delete/${id}`,
   UPDATE: (id: string) => `/api/update/${id}`,
 };
