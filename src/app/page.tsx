977662ece3ffc4f06d7b71f668ab41481b1866e2
choichangyeon <changyon99@naver.com>
2025-05-13 19:53:09 +0900

    feat:#1 prefetch 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
+import { QUERY_KEY } from "@/constants/query-key";
+import { getTodos } from "@/features/read/api/server-services";
+import { QueryClient } from "@tanstack/react-query";
+
+const { TODOS } = QUERY_KEY;
+
+export const Home = async () => {
+  const queryClient = new QueryClient();
+
+  queryClient.prefetchQuery({
+    queryKey: [TODOS],
+    queryFn: getTodos,
+  });
+
   return <div>Hello World</div>;
-}
+};
+
+export default Home;
