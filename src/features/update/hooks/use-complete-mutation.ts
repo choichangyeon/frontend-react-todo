e0a5507a686312d669f6a7553f84c699ebfd5bd2
choichangyeon <changyon99@naver.com>
2025-05-14 01:53:14 +0900

    feat:#1 tanstack query completed - patch

---
+++ src/features/update/hooks/use-complete-mutation.ts
@@ -0,0 +1,40 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateTodo } from "@/features/update/api/client-services";
+import { Todo } from "@/types/todo";
+import { QUERY_KEY } from "@/constants/query-key";
+
+const { TODOS } = QUERY_KEY;
+type Props = Pick<Todo, "id">;
+
+const useCompleteMutation = ({ id }: Props) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (completed: boolean) =>
+      updateTodo({ id, type: "completed", completed }),
+    onMutate: async (completed) => {
+      await queryClient.cancelQueries({ queryKey: [TODOS] });
+      const previousTodos = queryClient.getQueryData<Todo[]>([TODOS]) ?? [];
+
+      queryClient.setQueryData(
+        [TODOS],
+        previousTodos.map((todo) =>
+          todo.id === id ? { ...todo, completed } : todo
+        )
+      );
+
+      return { previousTodos };
+    },
+    onError: (error, _variables, context) => {
+      if (context?.previousTodos) {
+        queryClient.setQueryData([TODOS], context.previousTodos);
+      }
+      throw new Error(error.message);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [TODOS] });
+    },
+  });
+};
+
+export default useCompleteMutation;
