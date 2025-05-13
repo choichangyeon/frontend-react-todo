f100530f9dda1cd561c5beeef961381cd777048d
choichangyeon <changyon99@naver.com>
2025-05-13 19:48:09 +0900

    feat:#1 tanstack-query 지정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import TanstackQueryProvider from "@/provider/tanstack-query-provider";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -5,14 +6,18 @@ export const metadata: Metadata = {
   description: "React Todo App 바로인턴 12기 과제",
 };
 
-export default function RootLayout({
+export const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body>
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
+      </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
