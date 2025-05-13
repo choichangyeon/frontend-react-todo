086da81b6f629abd9000f9d04dbd9b5fd57e0856
choichangyeon <changyon99@naver.com>
2025-05-14 01:16:22 +0900

    refactor:#1 type 맞추기

--- src/features/delete/api/client-services.ts
+++ src/features/delete/api/client-services.ts
@@ -1,13 +1,14 @@
 import { API_METHOD, API_ROUTE_HANDLER_PATH } from "@/constants/api";
+import { Todo } from "@/types/todo";
 
 const { DELETE } = API_ROUTE_HANDLER_PATH;
 const { DELETE: DELETE_METHOD } = API_METHOD;
 
-export const deleteTodo = async (id: string) => {
+export const deleteTodo = async (id: string): Promise<Todo> => {
   const url = DELETE(id);
   const res = await fetch(url, {
     method: DELETE_METHOD,
   });
   const { data } = await res.json();
-  return data;
+  return data as Todo;
 };
