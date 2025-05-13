2c626a427c6487ad32191895c05f886a534faed7
choichangyeon <changyon99@naver.com>
2025-05-14 03:06:44 +0900

    refactor:#1 header 추가

--- src/features/delete/api/client-services.ts
+++ src/features/delete/api/client-services.ts
@@ -1,13 +1,19 @@
-import { API_METHOD, API_ROUTE_HANDLER_PATH } from "@/constants/api";
+import {
+  API_HEADER,
+  API_METHOD,
+  API_ROUTE_HANDLER_PATH,
+} from "@/constants/api";
 import type { Todo } from "@/types/todo";
 
 const { DELETE } = API_ROUTE_HANDLER_PATH;
 const { DELETE: DELETE_METHOD } = API_METHOD;
+const { JSON_HEADER } = API_HEADER;
 
 export const deleteTodo = async (id: string): Promise<Todo> => {
   const url = DELETE(id);
   const res = await fetch(url, {
     method: DELETE_METHOD,
+    headers: JSON_HEADER,
   });
   const { data } = await res.json();
   return data as Todo;
