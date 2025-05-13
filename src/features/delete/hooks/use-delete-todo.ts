225edcaa9d4441437c2afd36c42d95c0a240f044
choichangyeon <changyon99@naver.com>
2025-05-13 23:23:52 +0900

    refactor:#1 id props

--- src/features/delete/hooks/use-delete-todo.ts
+++ src/features/delete/hooks/use-delete-todo.ts
@@ -2,14 +2,17 @@ import { useMutation } from "@tanstack/react-query";
 import { deleteTodo } from "@/features/delete/api/client-services";
 import { QUERY_KEY } from "@/constants/query-key";
 import { useQueryClient } from "@tanstack/react-query";
+import { Todo } from "@/types/todo";
 
 const { TODOS } = QUERY_KEY;
 
-export const useDeleteTodo = () => {
+type Props = Pick<Todo, "id">;
+
+export const useDeleteTodo = ({ id }: Props) => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: deleteTodo,
+    mutationFn: () => deleteTodo(id),
     onError: (error) => {
       throw new Error(error.message);
     },
