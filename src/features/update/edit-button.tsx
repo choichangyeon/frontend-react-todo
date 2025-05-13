d32938993b99a2fecb29ae7d8f8a40d6aee4b713
choichangyeon <changyon99@naver.com>
2025-05-14 02:47:12 +0900

    feat:#1 edit button 구현

---
+++ src/features/update/edit-button.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import useEditMutation from "@/features/update/hooks/use-edit-mutation";
+import type { Todo } from "@/types/todo";
+
+type Props = Omit<Todo, "completed"> & {
+  editTitle?: string;
+  isEditing: boolean;
+  onSetEditing: (isEditing: boolean) => void;
+};
+
+const EditButton = ({
+  id,
+  title,
+  isEditing,
+  onSetEditing,
+  editTitle,
+}: Props) => {
+  const { mutateAsync: updateTodo } = useEditMutation({ id });
+
+  const handleEdit = async () => {
+    if (!isEditing) {
+      onSetEditing(true);
+      return;
+    }
+    if (isEditing && title === editTitle) {
+      onSetEditing(false);
+      return;
+    }
+    if (isEditing && !editTitle) {
+      onSetEditing(false);
+      return;
+    }
+    if (isEditing && editTitle) {
+      try {
+        await updateTodo(editTitle);
+        onSetEditing(false);
+        alert("해당 Todo가 수정되었습니다.");
+      } catch (error) {
+        alert(error);
+      }
+    }
+  };
+  return <button onClick={handleEdit}>수정</button>;
+};
+
+export default EditButton;
