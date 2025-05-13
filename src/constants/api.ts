15c546b99fea3b0768a8bf99a002ac78986bce13
choichangyeon <changyon99@naver.com>
2025-05-13 20:30:44 +0900

    feat:#1 header & method 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -4,9 +4,16 @@ export const API_METHOD = {
   GET: "GET",
   POST: "POST",
   PUT: "PUT",
+  PATCH: "PATCH",
   DELETE: "DELETE",
 };
 
 export const API_ROUTE_HANDLER_PATH = {
   READ: "/api/read",
+};
+
+export const API_HEADER = {
+  JSON_HEADER: {
+    "Content-Type": "application/json",
+  },
 };
