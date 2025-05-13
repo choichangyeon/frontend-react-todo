9c32791f41b00d9d3ccf80e08d711e6c4c690882
choichangyeon <changyon99@naver.com>
2025-05-13 20:24:51 +0900

    feat:#1 api constants 설정

---
+++ src/constants/api.ts
@@ -0,0 +1,12 @@
+export const API_URL = "http://localhost:3000";
+
+export const API_METHOD = {
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  DELETE: "DELETE",
+};
+
+export const API_ROUTE_HANDLER_PATH = {
+  READ: "/api/read",
+};
