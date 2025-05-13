7a6f8da6c09795528dd20617f2d4810624534b10
choichangyeon <changyon99@naver.com>
2025-05-13 20:39:50 +0900

    feat:#1 route handler - read

---
+++ src/features/read/api/client-services.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import {
+  API_HEADER,
+  API_METHOD,
+  API_ROUTE_HANDLER_PATH,
+} from "@/constants/api";
+import { Todo } from "@/types/todo";
+
+const { READ } = API_ROUTE_HANDLER_PATH;
+const { GET } = API_METHOD;
+const { JSON_HEADER } = API_HEADER;
+
+export const getTodos = async (): Promise<Todo[]> => {
+  const res = await fetch(READ, {
+    method: GET,
+    headers: JSON_HEADER,
+  });
+  const data: Todo[] = await res.json();
+
+  return data;
+};
