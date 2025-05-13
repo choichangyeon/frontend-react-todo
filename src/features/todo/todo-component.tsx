13ca1ed8c94dd2b3c4ce4c4fa99e06af87d55f2c
choichangyeon <changyon99@naver.com>
2025-05-14 03:38:51 +0900

    feat:#1 Todo 컴포넌트 구현

--- src/features/todo/todo-component.tsx
+++ src/features/todo/todo-component.tsx
@@ -1,9 +1,41 @@
-import { Todo } from "@/types/todo";
+"use client";
 
-type Props = Omit<Todo, "id">;
+import { useState } from "react";
+import CompleteButton from "@/features/update/complete-button";
+import DeleteButton from "@/features/delete/delete-button";
+import EditButton from "@/features/update/edit-button";
+import type { Todo } from "@/types/todo";
 
-const TodoComponent = ({ completed, title }: Props) => {
-  return <li>{title}</li>;
+type Props = Todo;
+
+const TodoComponent = ({ completed, title, id }: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editTitle, setEditTitle] = useState(title);
+
+  return (
+    <li className="flex flex-row items-center">
+      {isEditing ? (
+        <input
+          type="text"
+          value={editTitle}
+          onChange={(e) => setEditTitle(e.target.value)}
+        />
+      ) : completed ? (
+        <s>{title}</s>
+      ) : (
+        <span>{title}</span>
+      )}
+      <CompleteButton id={id} completed={completed} />
+      <DeleteButton id={id} />
+      <EditButton
+        id={id}
+        isEditing={isEditing}
+        onSetEditing={setIsEditing}
+        editTitle={editTitle}
+        title={title}
+      />
+    </li>
+  );
 };
 
 export default TodoComponent;
