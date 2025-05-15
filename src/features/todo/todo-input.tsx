11bf576a189fd78a362292f436e2f1b7033b49c6
choichangyeon <changyon99@naver.com>
2025-05-15 11:29:49 +0900

    style:#3 input & button UI

--- src/features/todo/todo-input.tsx
+++ src/features/todo/todo-input.tsx
@@ -6,11 +6,13 @@ import CreateButton from "@/features/create/create-button";
 const TodoInput = () => {
   const [title, setTitle] = useState("");
   return (
-    <section>
+    <section className="flex flex-row gap-2 mb-4">
       <input
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
+        className="border border-gray-300 rounded-md p-2 w-52"
+        placeholder="Todo를 입력해주세요."
       />
       <CreateButton title={title} onSetTitle={setTitle} />
     </section>
