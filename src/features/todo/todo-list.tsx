a4164af8d85bf407bc46ddd14f14c60f185f7c9c
choichangyeon <changyon99@naver.com>
2025-05-15 09:46:53 +0900

    feat:#2 필터링 상태 적용

--- src/features/todo/todo-list.tsx
+++ src/features/todo/todo-list.tsx
@@ -2,20 +2,25 @@
 
 import { useGetTodosQuery } from "@/features/read/hooks/use-get-todos-query";
 import TodoComponent from "@/features/todo/todo-component";
+import { useState } from "react";
+import FilterSelectBox from "@/features/read/filter-select-box";
 
 const TodoList = () => {
   const { data: todos, isPending, isError } = useGetTodosQuery();
+  const [filter, setFilter] = useState<"all" | "completed">("all");
 
   if (isPending) return <div>로딩 중...</div>;
   if (isError) return <div>todos 요청 오류</div>;
 
-  console.log(todos);
   return (
     <section>
+      <FilterSelectBox filter={filter} setFilter={setFilter} />
       <ul>
-        {todos.map((todo) => (
-          <TodoComponent key={todo.id} {...todo} />
-        ))}
+        {filter === "all"
+          ? todos.map((todo) => <TodoComponent key={todo.id} {...todo} />)
+          : todos
+              .filter((todo) => todo.completed)
+              .map((todo) => <TodoComponent key={todo.id} {...todo} />)}
       </ul>
     </section>
   );
